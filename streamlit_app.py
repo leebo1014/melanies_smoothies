e2660bdd54ff30f9ba6aa652a10c8ea13ee327f2
leebo1014 <34902084+leebo1014@users.noreply.github.com>
2025-03-06 09:58:05 +0800

    Update streamlit_app.py
    change to api url

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,11 +53,15 @@ my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_ord
 
 #st.write(my_insert_stmt)
 
-time_to_insert=st.button("Submit Order")
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
+# time_to_insert=st.button("Submit Order")
+
+# if time_to_insert:
+#     session.sql(my_insert_stmt).collect()
+#     st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
 
 
 
